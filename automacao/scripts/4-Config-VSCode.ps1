d5b4d0d107bdf687eac3f799d660dda91976be94
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2023-05-25 01:54:15 -0300

    Script -  Instalar Extensões, Copiar Configurações

---
+++ automacao/scripts/4-Config-VSCode.ps1
@@ -0,0 +1,36 @@
+# Instalar extensões
+function Install-Extensions {
+  code --install-extension danielpinto8zz6.c-cpp-compile-run --force
+  code --install-extension ms-vscode.cpptools-extension-pack --force
+}
+Install-Extensions
+
+# Log status
+Write-Host -ForegroundColor Yellow "(1/2) Extensoes Instaladas"
+
+# Copiar Configurações
+function Copy-Configs {
+  # Caminhos das configurações
+  $paths = @(
+    ".\configs\cpp.json",
+    ".\configs\keybindings.json"
+  )
+  $destinations = @(
+    "$env:APPDATA\Code\User\snippets\",
+    "$env:APPDATA\Code\User\"
+  )
+    
+  # Copiando configurações
+  for ($i = 0; $i -lt $paths.Count; $i++) {
+    if (Test-Path $paths[$i]) { 
+      Copy-Item -Path $paths[$i] -Destination $destinations[$i]
+    }
+    else {
+      Throw "Arquivo '$file' não encontrado."
+    }
+  }
+}
+Copy-Configs
+
+# Log status
+Write-Host -ForegroundColor Yellow "(2/2) VS Code Configurado"
